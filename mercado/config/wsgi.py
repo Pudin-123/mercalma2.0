f774f29213cdfe6cb713d8667011e770a20158f2
Pudin-123 <azcagorta800@gmail.com>
2025-12-03 13:46:25 -0300

    Auto-run migrations on WSGI startup to ensure DB schema is always current

--- mercado/config/wsgi.py
+++ mercado/config/wsgi.py
@@ -1,6 +1,13 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+# Auto-run migrations on app startup (safe for production)
+try:
+    call_command('migrate', '--noinput', verbosity=0)
+except Exception as e:
+    print(f'Warning: Could not run migrations on startup: {e}')
